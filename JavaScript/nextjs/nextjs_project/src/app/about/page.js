19e2913facb56824f544ac087b75ada7a1ecf16c
AddisonReyes <addison.amin@gmail.com>
2026-05-14 12:58:45 -0400

    Removing JavaScript folder

--- JavaScript/nextjs/nextjs_project/src/app/about/page.js
+++
@@ -1,4 +0,0 @@
-// src/app/about/page.js (route: /about)
-export default function AboutPage() {
-  return <h1>About Us</h1>;
-}
--- JavaScript/nextjs/nextjs_project/src/app/page.js
+++
@@ -1,8 +0,0 @@
-// src/app/page.js (route: /)
-export default function Home() {
-  return (
-    <div>
-      <h1>Next.js 16 Architecture</h1>
-    </div>
-  );
-}
