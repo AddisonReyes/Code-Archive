4e663adfb7cc820feff8d5ef6ea840c9e167f99a
AddisonReyes <addison.amin@gmail.com>
2026-05-29 06:59:39 -0400

    [Rust/Book] Relative/Absolute module paths, and pub enums, structs, and methods

--- Rust/restaurant/src/lib.rs
+++ Rust/restaurant/src/lib.rs
@@ -1,26 +1,72 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
+        #[allow(dead_code)]
         fn seat_at_table() {}
     }
 
     mod serving {
+        #[allow(dead_code)]
         fn take_order() {}
 
+        #[allow(dead_code)]
         fn serve_order() {}
 
+        #[allow(dead_code)]
         fn take_payment() {}
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[allow(dead_code)]
+fn deliver_order() {}
 
-    #[test]
-    fn it_works() {
-        // let result = add(2, 2);
-        // assert_eq!(result, 4);
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    #[allow(dead_code)]
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    #[allow(dead_code)]
+    fn cook_order() {}
+}
+
+#[allow(unused)]
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    #[allow(unused)]
+    front_of_house::hosting::add_to_waitlist();
 }
